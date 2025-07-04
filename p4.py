42c4956b7f8e85456de29bda1b5ae78ac3750718
hashim-pixel266 <hashim5911dgaf@icloud.com>
2025-07-04 16:46:47 +0500

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,26 @@
+print("hashim")
+
+age = 18
+print(age)
+
+sname = "AI HUB"
+print(sname)
+
+a=10
+b=20
+print(a+b)
+
+name = "hashim"
+print(f"Wellcome {name}")
+
+city = "Gujranwala"
+print (city)
+
+print(f"My name is {name} & my age is {age} & i live in {city}")
+
+print(a-b)
+
+print("Do good End good")
+
+fsub = "Computer"
+print (fsub)
